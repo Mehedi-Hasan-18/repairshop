9f0c9bcf8192e292e1178e30ff721f3e504de497
Mehedi-Hasan-18 <mdmehedihasanroby@gmail.com>
2026-04-09 10:21:27 +0600

    Use kinde for authenticaiton and Login

--- app/(rs)/home/page.tsx
+++ app/(rs)/home/page.tsx
@@ -1,10 +1,18 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:"Home"
+  title: "Home",
 };
 
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
-export default function Home(){
-    return <h2>Home Page</h2>
+export default function Home() {
+  return (
+    <>
+      <h2>Home Page</h2>
+    </>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="bg-black bg-[url('/home-img.webp')] bg-cover bg-center">
-
+    <div
+      className="bg-black  bg-cover bg-center"
+      style={{ backgroundImage: "url('/home-img.webp')" }}
+    >
       <main className="flex flex-col justify-center text-center max-w-5xl h-dvh mx-auto">
         <div className="flex-flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
           <h1 className="text-3xl font-bold">
             Mehedi's Computer <br /> Repair Shop
           </h1>
-          <address>
-            Rangpur
-          </address>
-          <p>
-            Open : Daily 9AM - 5PM
-          </p>
-          <Link href={"tel:01626759496"} className="hover:underline">01626759496</Link>
+          <address>Rangpur</address>
+          <p>Open : Daily 9AM - 5PM</p>
+          <Link href={"tel:01626759496"} className="hover:underline">
+            01626759496
+          </Link>
         </div>
       </main>
     </div>
